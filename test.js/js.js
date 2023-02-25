3e2c6a25f0bf2b16ebf594fa5b5e9c7c5ae27fe6
Artem311982 <110343485+Artem311982@users.noreply.github.com>
2023-02-25 13:10:53 +0200

    250223

--- test.js/js.js
+++ test.js/js.js
@@ -1,2 +1,67 @@
-var declaration;
-console.log(declaration);
+// // Setup
+// let count = 0;
+
+// function cc(card) {
+//   // Only change code below this line
+
+// switch (card){
+//     case 2:
+//     case 3:
+//     case 4:
+//     case 5:
+//     case 6:
+//         count += 1;
+//         break;
+
+//     case 10:
+//     case "J":
+//     case "Q":
+//     case "K":
+//     case "A":
+//         count -= 1;
+//         break;
+// }
+// var holdbet = 'Hold';
+// if(count > 0){
+//   holdbet = "Bet";
+// }
+//   return count + " " + holdbet;
+//   // Only change code above this line
+// }
+
+// cc(6); cc(3); cc(7); cc('K'); cc('A');
+//   console.log(cc());
+
+//   console.log(count);
+
+
+
+
+// const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+// const a = names.length({name, index, "Par"});
+// function golfScore(par, strokes) {
+//   // Only change code below this line
+// if(strokes == 1){
+// return names[0];
+// } else if(strokes <= par -2){
+// return names[1];
+// } else if(strokes == par -1){
+//   return names[2];
+// } else if(strokes == par){
+//   return names[3];
+// } else if(strokes == par +1){
+//   return names[4];
+// } else if(strokes == par +2){
+//   return names[5];
+// } else if(strokes >= par +3){
+//   return names[6];
+// } else {
+//   return "Change Me"
+// };
+//   // Only change code above this line
+// }
+
+// golfScore(5, 4);
+//   // Display code
+//   console.log(golfScore());
+//   console.log(a)
---
+++ test.js/onemorejs).js
@@ -0,0 +1,30 @@
+const a = 39.9-37.46;
+
+const b = a / 37.46 *100;
+
+console.log(a);
+
+console.log(b);
+
+
+
+// Setup
+// function phoneticLookup(key) {
+//     let result = "";
+  
+//     // Only change code below this line
+//     const lookup = {
+//     "alpha": "Adams",
+//     "bravo": "Boston",
+//     "charlie": "Chicago",
+//     "delta": "Denver",
+//     "echo": "Easy",
+//     "foxtrot": "Frank",
+//     }
+//   result = lookup[key]
+//     // Only change code above this line
+//     return result;
+//   }
+  
+//   console.log(phoneticLookup("foxtrot"));
+
